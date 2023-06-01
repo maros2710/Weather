db297e6b5fa8b8c50f3b070af694184af13cc5d8
Marian Hacaj <marian.hacaj@profinit.eu>
2023-06-01 13:38:07 +0200

    Small fixes

--- src/main/resources/static/main.js
+++ src/main/resources/static/main.js
@@ -9,18 +9,22 @@ $(document).ready(function() {
 
         let weather = weatherData[d];
 
-        $('#weather').append('<div><img src="' + weather.icon + '"></div>');
-        $('#weather').append('<div>Pocasie:&nbsp;' + weather.text + '</div>');
-        $('#weather').append('<div>Teplota:&nbsp;' + weather.tempC + '</div>');
-        $('#weather').append('<div>Pocitova teplota:&nbsp;' + weather.feelsLikeC + '</div>');
-        $('#weather').append('<div>Oblaky:&nbsp;' + weather.cloud + '</div>');
-        $('#weather').append('<div>Zrazky (mm):&nbsp;' + weather.precipMM + '</div>');
-        $('#weather').append('<div>Vietor (km/h):&nbsp;' + weather.windKph + '</div>');
-        $('#weather').append('<div>Narazy (km/h):&nbsp;' + weather.gustKph + '</div>');
-        $('#weather').append('<div>Smer vetra:&nbsp;' + weather.windDir + '</div>');
-        $('#weather').append('<div>Vlhkost:&nbsp;' + weather.humidity + '</div>');
-        $('#weather').append('<div>Tlak (MB):&nbsp;' + weather.pressureMB + '</div>');
-        $('#weather').append('<div>UV index:&nbsp;' + weather.uv + '</div>');
+        if(weather) {
+            $('#weather').append('<div><img src="' + weather.icon + '"></div>');
+            $('#weather').append('<div>Pocasie:&nbsp;' + weather.text + '</div>');
+            $('#weather').append('<div>Teplota:&nbsp;' + weather.tempC + '</div>');
+            $('#weather').append('<div>Pocitova teplota:&nbsp;' + weather.feelsLikeC + '</div>');
+            $('#weather').append('<div>Oblaky:&nbsp;' + weather.cloud + '</div>');
+            $('#weather').append('<div>Zrazky (mm):&nbsp;' + weather.precipMM + '</div>');
+            $('#weather').append('<div>Vietor (km/h):&nbsp;' + weather.windKph + '</div>');
+            $('#weather').append('<div>Narazy (km/h):&nbsp;' + weather.gustKph + '</div>');
+            $('#weather').append('<div>Smer vetra:&nbsp;' + weather.windDir + '</div>');
+            $('#weather').append('<div>Vlhkost:&nbsp;' + weather.humidity + '</div>');
+            $('#weather').append('<div>Tlak (MB):&nbsp;' + weather.pressureMB + '</div>');
+            $('#weather').append('<div>UV index:&nbsp;' + weather.uv + '</div>');
+        } else {
+            $('#weather').append('<div>Ziadne data</div>');
+        }
     };
 
     let load = function() {
@@ -47,7 +51,8 @@ $(document).ready(function() {
     });
 
     $('#date').datepicker({
-        dateFormat: "dd.mm.yy"
+        dateFormat: "dd.mm.yy",
+        minDate: new Date(2023, 5 - 1, 31)
     });
     $('#date').datepicker('setDate', 'today');
 
